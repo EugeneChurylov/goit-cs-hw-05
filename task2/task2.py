9f401f7b6633319c0c3fd24958dee2cf079487ad
EugeneChurylov <e.churylov@gmail.com>
2024-03-18 17:11:05 +0100

    додав багатопотоковість

--- task2/task2.py
+++ task2/task2.py
@@ -2,19 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 from collections import Counter
 import matplotlib.pyplot as plt
-
+from concurrent.futures import ThreadPoolExecutor
 
 def mapper(text):
     words = text.split()  # Розбиваємо текст на слова
     return [(word.lower(), 1) for word in words]
-
 
 def reducer(mapped_items):
     word_counts = Counter()
     for word, count in mapped_items:
         word_counts[word] += count
     return word_counts.items()
-
 
 def visualize_top_words(word_counts, top_n=10):
     top_words = word_counts.most_common(top_n)
@@ -28,18 +26,23 @@ def visualize_top_words(word_counts, top_n=10):
     plt.tight_layout()
     plt.show()
 
-
-def main(url):
+def process_url(url):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
         visible_text = soup.get_text()
         mapped_items = mapper(visible_text)
         word_counts = reducer(mapped_items)
-        visualize_top_words(Counter(dict(word_counts)))
+        return Counter(dict(word_counts))
     else:
         print("Failed to fetch URL")
+        return Counter()
 
+def main(url):
+    with ThreadPoolExecutor() as executor:
+        future = executor.submit(process_url, url)
+        word_counts = future.result()
+        visualize_top_words(word_counts)
 
 if __name__ == "__main__":
     url = "https://www.python.org/"
